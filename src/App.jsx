f5f0f8b83a7289b330746203399169bc4db4d396
Ibrahim Sadudin Kedir <ibrahimsdudin@gmail.com>
2022-09-08 11:11:31 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,322 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import mountEverest from './assets/myAssets/katie-moum-iRMUDX0kyOc-unsplash.jpg'
+import coffee from './assets/myAssets/nathan-lemon-FBiKcUw_sQw-unsplash.jpg'
+import Kapadokiyya from './assets/myAssets/ian-dooley-hpTH5b6mo2s-unsplash.jpg'
+import iceCream from './assets/myAssets/ian-dooley-TLD6iCOlyb0-unsplash.jpg'
+
+import the80s from './assets/myAssets/ajeet-mestry-UBhpOIHnazM-unsplash.jpg'
+import Rose from './assets/myAssets/tirza-van-dijk-cNGUw-CEsp0-unsplash.jpg'
+import Beach from './assets/myAssets/bekir-donmez-eofm5R5f9Kw-unsplash.jpg'
+import Sky from './assets/myAssets/laura-vinck-Hyu76loQLdk-unsplash.jpg'
+
+
+
+
+
+ import './App.css'
+
+function App() {
+ 
+  return (
+    <div className="App">
+
+
+
+      <div className="body">
+
+
+          <div className="container">
+
+  
+
+              <div className="pic">
+
+                    <div className="left">
+                        <img src={mountEverest} alt="Smiley face" width="150" height="150" />
+                    </div>
+ 
+                        <div className="right">
+
+                            <div className="explanation-container">
+
+                                <p className="title">
+
+                                    Mount Everest
+                                </p>
+
+                                <p className="explanation">
+
+                                    this is that one place every one should go to chajnge the way their life...
+                                    this is that one place every one should go to chajnge the way their life...
+
+
+
+                                </p>
+
+                            </div>
+                          
+                            
+                        </div> 
+
+                    </div>  
+
+ 
+                                
+                     
+
+                     <div className="pic">
+
+                    <div className="left">
+                        <img src={coffee} alt="Smiley face" width="150" height="150" >
+                        </img>
+                    </div>
+
+                    <div className="right">
+
+                        <div className="explanation-container">
+
+                            <p className="title">
+
+                                coffee time
+                            </p>
+
+                            <p className="explanation">
+
+                                coffee time in our country is the most important part of the day...
+
+                            </p>
+
+                        </div>
+                        
+                    </div>
+
+                    </div>
+
+                    <div className="pic">
+                    <div className="left">
+                        <img src={Kapadokiyya} alt="Smiley face" width="150" height="150" >
+                        </img>
+                    </div>
+
+                    <div className="right">
+
+                        <div className="explanation-container">
+
+                            <p className="title">
+
+                                Kapadokiyya
+                            </p>
+
+                            <p className="explanation">
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Nunc rhoncus varius lacinia. Nullam justo nunc, eleifend 
+                                vel vestibulum ac, tristique vel risus. Nunc ut mi sed diam 
+
+
+                            </p>
+
+                        </div>
+                        
+                    </div>
+                    </div>
+
+                    <div className="pic">
+
+                    <div className="left">
+                        <img src={iceCream} alt="Smiley face" width="150" height="150" ></img>
+
+                    </div>
+
+                    <div className="right">
+
+                        <div className="explanation-container">
+
+                            <p className="title">
+
+                                Ice cream
+                            </p>
+
+                            <p className="explanation">
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Nunc rhoncus varius lacinia. Nullam justo nunc, eleifend 
+
+                            </p>
+
+                        </div>
+                        
+                    </div>
+
+                    </div>
+
+                    <div className="pic">
+
+                    <div className="left">
+                        <img src={Rose} alt="Smiley face" width="150" height="150" >
+                        </img>
+                    </div>
+
+                    <div className="right">
+
+                        <div className="explanation-container">
+
+                            <p className="title">
+
+                                Rose
+                            </p>
+
+                            <p className="explanation">
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Nunc rhoncus varius lacinia. Nullam justo nunc, eleifend 
+                                vel vestibulum ac, tristique vel risus. Nunc ut mi sed diam     
+                            </p>
+
+                        </div>
+                        
+                    </div>
+
+                    </div>
+
+                    <div className="pic">
+
+                    <div className="left">
+                        <img src={the80s} alt="Smiley face" width="150" height="150" >
+                        </img>
+                    </div>
+
+                    <div className="right">
+
+                        <div className="explanation-container">
+
+                            <p className="title">
+
+                                80's
+                            </p>
+
+                            <p className="explanation">
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Nunc rhoncus varius lacinia. Nullam justo nunc, eleifend 
+                                vel vestibulum ac, tristique vel risus. Nunc ut mi sed diam 
+
+                            </p>
+
+                        </div>
+                        
+                    </div>
+
+                    </div>
+
+                    <div className="pic">
+
+                    <div className="left">
+                        <img src={Beach} alt="Smiley face" width="150" height="150" >
+                          
+                          </img>
+
+                    </div>
+
+                    <div className="right">
+
+                        <div className="explanation-container">
+
+                            <p className="title">
+
+                                Beach
+                            </p>
+
+                            <p className="explanation">
+
+                                Nunc rhoncus varius lacinia. Nullam justo nunc, eleifend 
+                                vel vestibulum ac, tristique vel risus. Nunc ut mi sed diam     
+                            </p>
+
+                        </div>
+                        
+                    </div>
+
+                    </div>
+
+                    <div className="pic">
+
+                    <div className="left">
+                        <img src={Sky} alt="Smiley face" width="150" height="150" >
+                        </img>
+                    </div>
+
+                    <div className="right">
+
+                        <div className="explanation-container">
+
+                            <p className="title">
+
+                                Sky
+                            </p>
+
+                            <p className="explanation">
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Nunc rhoncus varius lacinia. Nullam justo nunc, eleifend 
+                            </p>
+
+                        </div>
+                        
+                    </div>
+
+                    </div>
+
+
+                    <div className="pic">
+
+                    <div className="left">
+                        <img src={Sky} alt="Smiley face" width="150" height="150" >
+                        </img>
+                    </div>
+
+                    <div className="right">
+
+                        <div className="explanation-container">
+
+                            <p className="title">
+
+                                Sky
+                            </p>
+
+                            <p className="explanation">
+
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+
+                            </p>
+
+                        </div>
+                        
+                    </div>
+
+                    </div> 
+
+
+
+ 
+          </div>
+
+      </div>
+
+
+
+
+
+
+
+
+      
+      
+      
+    </div>
+  )
+}
+
+export default App
